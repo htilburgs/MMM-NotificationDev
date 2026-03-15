100e06c14f7895c8b944409d3498e43bae5fddfa
Harm Tilburgs <harm@tilburgs.com>
2026-03-15 15:15:33 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -3,23 +3,16 @@ const NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
 
     start: function () {
-        console.log("MMM-NotificationDev node helper started");
+        console.log("MMM-NotificationDev NodeHelper started");
     },
 
-    notificationReceived: function (notification, payload, sender) {
-
-        const senderName = sender && sender.name ? sender.name : "SYSTEM";
-
-        console.log("[NotificationDev NODE]", notification, payload);
-
+    notificationReceived: function(notification, payload, sender) {
+        const senderName = sender?.name || "NODE";
         this.sendSocketNotification("NODE_NOTIFICATION", {
-            time: new Date().toLocaleTimeString(),
-            notification: notification,
+            notification,
             payload: payload ?? null,
-            sender: senderName,
-            source: "NODE"
+            sender: senderName
         });
-
     }
 
 });
